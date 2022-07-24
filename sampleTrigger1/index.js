2ca4792663233391f8a41869ee92f93bffb29406
Hidenori Miyoshi <miyohide@gmail.com>
2022-07-24 20:35:21 +0900

    add comments

--- sampleTrigger1/index.js
+++ sampleTrigger1/index.js
@@ -4,6 +4,8 @@ module.exports = async function (context, req) {
     const startTime = new Date();
     context.log('JavaScript HTTP trigger function processed a request.');
 
+    // HTTPトリガーが230秒で502となることを確認するテスト
+    // 一定時間止まって、定期的にログを出力することを指定回数実施する
     for(let i = 0; i < 270; i++) {
         await setTimeout(1000);
         context.log(`Waiting... ${i}`);
